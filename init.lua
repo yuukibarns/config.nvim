df51369a59080db8c9d525615dff867d99588200
yuukibarns <snrabikuuy@sina.com>
2024-06-30 21:46:16 +0800

    change default terminal to pwsh

--- init.lua
+++ init.lua
@@ -6,6 +6,20 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 
+-- POWERSHELL
+local powershell_options = {
+	shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+	shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+	shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+	shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+	shellquote = "",
+	shellxquote = "",
+}
+
+for option, value in pairs(powershell_options) do
+	vim.opt[option] = value
+end
+
 ---------- LAZYINIT ----------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
@@ -41,4 +55,3 @@ require("lazy").setup({
 		},
 	},
 })
-
