23f019ac273e50c0d8785162cb475a30671b4c39
yuukibarns <yuukibarns@gmail.com>
2024-09-24 18:08:21 +0800

    chores

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -158,16 +158,6 @@ return {
 		"stevearc/conform.nvim",
 		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 		dependencies = { "mason.nvim" },
-		-- keys = {
-		-- 	{
-		-- 		"<leader>a",
-		-- 		function()
-		-- 			require("conform").format({ formatters = { "injected" } })
-		-- 		end,
-		-- 		mode = { "n", "v" },
-		-- 		desc = "Format Injected Langs",
-		-- 	},
-		-- },
 		keys = {
 			{
 				"<leader>a",
@@ -181,9 +171,7 @@ return {
 		opts = {
 			formatters_by_ft = {
 				bib = { "bibtex-tidy" },
-				--markdown = { "prettierd", "injected" },
 				markdown = { "prettier" },
-				--["markdown.mdx"] = { "prettierd", "injected" },
 				lua = { "stylua" },
 				tex = { "latexindent" },
 				python = { "ruff_format" },
@@ -194,16 +182,6 @@ return {
 				if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
 					return
 				end
-				-- Disable with a global or buffer-local variable
-				-- if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-				-- 	return
-				-- end
-				-- Disable autoformat for files in a certain path
-				-- local bufname = vim.api.nvim_buf_get_name(bufnr)
-				-- if bufname:match("/node_modules/") then
-				-- 	return
-				-- end
-				-- ...additional logic...
 				return { timeout_ms = 1000, lsp_fallback = true }
 			end,
 			-- format_on_save = {
