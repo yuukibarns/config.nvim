765ac87c0d56d294f3c643ffe749cfa9e42239da
yuukibarns <yuukibarns@archlinux.localdomain>
2024-11-16 10:54:36 +0800

    Add `$` text-object in markdown.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -12,3 +12,36 @@ vim.api.nvim_buf_create_user_command(0, "FixDisplayMath", function()
 	vim.cmd("%s/\\\\\\[/$$/g")
 	vim.cmd("%s/\\\\\\]/$$/g")
 end, {})
+
+local chars = { "$" }
+
+for _, char in ipairs(chars) do
+	vim.api.nvim_buf_set_keymap(
+		0,
+		"v",
+		"i" .. char,
+		string.format(":<C-u>normal! T%svt%s<CR>", char, char),
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_buf_set_keymap(
+		0,
+		"v",
+		"a" .. char,
+		string.format(":<C-u>normal! F%svf%s<CR>", char, char),
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_buf_set_keymap(
+		0,
+		"n",
+		"di" .. char,
+		string.format(":<C-u>normal! T%svt%sd<CR>", char, char),
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_buf_set_keymap(
+		0,
+		"n",
+		"da" .. char,
+		string.format(":<C-u>normal! F%svf%sd<CR>", char, char),
+		{ noremap = true, silent = true }
+	)
+end
